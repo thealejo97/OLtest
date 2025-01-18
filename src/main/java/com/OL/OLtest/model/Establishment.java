1b4b407b2f1017a59b94aec99f90694708aa0832
Alejandro <alejandromontano@useit.co>
2025-01-18 15:27:13 -0500

    Adding establishment

---
+++ src/main/java/com/OL/OLtest/model/Establishment.java
@@ -0,0 +1,55 @@
+package com.OL.OLtest.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "establishments")
+public class Establishment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "merchant_id", nullable = false)
+    private Merchant merchant;
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Merchant getMerchant() {
+        return merchant;
+    }
+
+    public void setMerchant(Merchant merchant) {
+        this.merchant = merchant;
+    }
+}
---
+++ src/main/java/com/OL/OLtest/repository/EstablishmentRepository.java
@@ -0,0 +1,9 @@
+package com.OL.OLtest.repository;
+
+import com.OL.OLtest.model.Establishment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstablishmentRepository extends JpaRepository<Establishment, Long> {
+}
