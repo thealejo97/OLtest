0156732b4df77429abf535d528751b9ab1757942
Alejandro <alejandromontano@useit.co>
2025-01-20 08:10:26 -0500

    Adding patch to cors

--- src/main/java/com/OL/OLtest/config/SecurityConfig.java
+++ src/main/java/com/OL/OLtest/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); 
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); 
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")); 
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); 
         configuration.setAllowCredentials(true); 
 
